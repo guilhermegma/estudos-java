7722d3d770a6a2c3202407548ee8842d58884d3d
Guilherme <seu-email@exemplo.com>
2026-02-23 10:41:50 -0300

    adicionando projeto criando-classes

---
+++ alura-java/modulo-01-primeiras-aplicacoes/criando-classes/src/Estudos.java
@@ -0,0 +1,5 @@
+public class Estudos{
+    public static void main (String[] args){
+        System.out.println ("Estudos:\n1-Estudar Java\n2-Fazer anotações\n3-Fazer exercicios para melhor fixação");
+    }
+}
